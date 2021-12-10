c639095d2fed5db9a921ad22365db497002b9dcf
jesse <jessewjx@163.com>
2021-12-10 15:10:38 -0500

    1) SARSA 2) Add some comments
    #TODO
    1) plot the actions on the grid

--- MDP_solver.py
+++ MDP_solver.py
@@ -1,20 +1,20 @@
 import numpy as np
 import GridWorldMDP as MDP
 import matplotlib.pyplot as plt
-#TODO
+
+
 class Solver:
-
-
-    def __init__(self, epsilon, num_episodes, gamma, xdim, ydim):
+    def __init__(self, epsilon, num_episodes, alpha, gamma, xdim, ydim):
         self.epsilon = epsilon
         self.num_episodes = num_episodes
+        self.alpha = alpha
         self.gamma = gamma
         self.xdim = xdim
         self.ydim = ydim
         self.Qdict = {}
 
 
-    # to define current state as (s[0],s[1])
+
     def epsilon_greedy(self, state):
         random_seed = np.random.random()
 
@@ -27,6 +27,7 @@ class Solver:
 
         return action
 
+
     def init_Qdict(self):
         state_actions = []
         for x in range(0,self.xdim):
@@ -34,11 +35,9 @@ class Solver:
                 for a in ['Up', 'Down', 'Left','Right']:
                     state_actions.append( ((x,y), a) )
 
-
         for k in state_actions:
             self.Qdict[k] = 0
 
-        print(self.Qdict)
 
 
     def Q(self, state,action):
@@ -50,64 +49,133 @@ class Solver:
     def Qlearning(self):
         # initialize Q function
         self.init_Qdict()
-        reward_trj = []
+        cumReward_trajectory = []
+
+        # roll out trajectories
         for i in range(0, self.num_episodes):
-            # print(f"=========episode {i}========")
             reward_Episode = 0
 
             # random start state
+            # state in Q function is the position [x,y]
+            # MDP state is [x,y,t]
             startState = [np.random.randint(0, xdim-1), np.random.randint(0, ydim-1)]
             currState = startState.copy()
             MDP_state = startState.copy()
             time = 0
             MDP_state.append(time)
-            # print("MDP state:", MDP_state)
-            # print("current state:", currState)
 
-
+            # roll out an episode until reaching the end state
+            # the checking the MDP state that involves the time step
             while not MDP.IsEnd(MDP_state):
-                print(f"current MDP_state: {MDP_state}")
 
-                #eplison greedy actions
+                #each step take eplison greedy action
                 action = self.epsilon_greedy(currState)
-                # successor state
+
+                # successor state, reward in the MDP model
+                # succState_reward = [x,y,t,reward]
                 succState_reward = MDP.succAndReward(MDP_state, action)
                 succState = succState_reward[:2]
                 reward = float(succState_reward[-1])
 
-
+                # update value of Q function of current state,action
+                # with the reward and the value of successor state
                 succState_Qdict = [self.Q(succState, action) for action in MDP.actions(succState)]
-                self.Qdict[(tuple(currState),action)] = self.Qdict[(tuple(currState),action)] + self.gamma * (reward + max(succState_Qdict) - self.Qdict[(tuple(currState),action)])
+                self.Qdict[(tuple(currState),action)] = self.Qdict[(tuple(currState),action)] + self.alpha * (reward + self.gamma * max(succState_Qdict) - self.Qdict[(tuple(currState),action)])
 
+                # state transition
                 currState = succState.copy()
-
-
                 MDP_state = succState_reward.copy()[:3]
+
+                # collect reward along the trajectory
                 reward_Episode = reward_Episode + reward
 
-            # sum_reward_Ep = sum_reward_Ep + reward_Episode
-            # reward_trj.append(sum_reward_Ep/float(i+1))
-            reward_trj.append(reward_Episode)
 
-        return reward_trj
+            cumReward_trajectory.append(reward_Episode)
 
+        return cumReward_trajectory
+
+    def Sarsa(self):
+        # initialize Q function
+        self.init_Qdict()
+        cumReward_trajectory = []
+
+        # roll out trajectories
+        for i in range(0, self.num_episodes):
+            reward_Episode = 0
+
+            # random start state
+            # state in Q function is the position [x,y]
+            # MDP state is [x,y,t]
+            startState = [np.random.randint(0, xdim - 1), np.random.randint(0, ydim - 1)]
+            currState = startState.copy()
+            MDP_state = startState.copy()
+            time = 0
+            MDP_state.append(time)
+
+            # roll out an episode until reaching the end state
+            # the checking the MDP state that involves the time step
+            while not MDP.IsEnd(MDP_state):
+                # each step take eplison greedy action
+                action = self.epsilon_greedy(currState)
+
+                # successor state, reward in the MDP model
+                # succState_reward = [x,y,t,reward]
+                succState_reward = MDP.succAndReward(MDP_state, action)
+                succState = succState_reward[:2]
+                reward = float(succState_reward[-1])
+
+                # update value of Q function of current state,action
+                # with the reward and the Q value of successor state and epsilon greedy action
+                succAction = self.epsilon_greedy(succState)
+                self.Qdict[(tuple(currState), action)] = self.Qdict[(tuple(currState), action)] + self.alpha * (
+                            reward + self.gamma * self.Qdict[(tuple(succState), succAction)] - self.Qdict[
+                        (tuple(currState), action)])
+
+                # state transition
+                currState = succState.copy()
+                MDP_state = succState_reward.copy()[:3]
+
+                # collect reward along the trajectory
+                reward_Episode = reward_Episode + reward
+
+            cumReward_trajectory.append(reward_Episode)
+
+        return cumReward_trajectory
 
 
 
 if __name__ == '__main__':
     epsilon = 0.1
-    num_episodes = 500
+    num_episodes = 50
     gamma = 1
+    alpha = 1
     xdim = 12
     ydim = 4
-    gw = Solver(epsilon, num_episodes, gamma, xdim, ydim)
+    gw = Solver(epsilon, num_episodes, alpha, gamma, xdim, ydim)
 
-
-    reward_arr=np.zeros(num_episodes)
+    # Q learning
+    Qreward_arr=np.zeros(num_episodes)
+    # average the episodes rewards over runs
     for r in range(0,10):
-        reward_trj = gw.Qlearning()
-        reward_arr = reward_arr + np.array(reward_trj)
-    reward_arr = reward_arr/float(10)
-    x = [i for i in range(0,num_episodes)]
-    plt.plot(x,reward_arr)
+        Qreward_trj = gw.Qlearning()
+        Qreward_arr = Qreward_arr + np.array(Qreward_trj)
+    Qreward_arr = Qreward_arr/float(10)
+
+
+    # Sarsa learning
+    Sreward_arr=np.zeros(num_episodes)
+    for r in range(0,10):
+        Sreward_trj = gw.Sarsa()
+        Sreward_arr = Sreward_arr + np.array(Sreward_trj)
+    Sreward_arr = Sreward_arr/float(10)
+
+
+    # plot average reward as a function of episodes
+    x = [i for i in range(0, num_episodes)]
+    plt.plot(x,Qreward_arr,label = "Q learning")
+    plt.plot(x, Sreward_arr,label = "Sarsa")
+    plt.xlabel('episode count')
+    plt.ylabel('cummulative reward')
+    # plt.title('Interesting Graph\nCheck it out')
+    plt.legend()
     plt.show()
